566e9d56fb8e4110151d9a8a7d8b4092bb206063
Angela <angelavardanyan1998@gmail.com>
2020-03-15 22:34:56 +0400

    Fix: Removing unneccessary redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,11 @@
 import { applyMiddleware, createStore } from "redux";
-import thunk from "redux-thunk";
 import { logger } from "redux-logger";
 
 import rootReducer from "./reducers/";
 
 const store = createStore(
   rootReducer,
-  process.env.NODE_ENV !== "development"
-    ? applyMiddleware(thunk)
-    : applyMiddleware(thunk, logger)
+  process.env.NODE_ENV !== "development" ? applyMiddleware(logger) : null
 );
 export default store;
 
